3d55a4db0821a6ee1f3232fc12ee88c244c10dcf
IdaliaGpe <idaliaguadalupepucca@hotmail.com>
2020-11-17 10:03:57 -0700

    hh

--- src/components/EditProductGroup.jsx
+++ src/components/EditProductGroup.jsx
@@ -2,12 +2,45 @@ import React, {Component} from "react";
 import {getApolloContext, gql} from '@apollo/client';
 import { Button, Divider, Header, Icon, List, ListContent, ListItem, Form, FormGroup, FormField, Input, FormButton, FormInput} from 'semantic-ui-react'
 
+const UPDATE_PRODUCT_GROUP = gql`
+    mutation($id:ID!, $name:String!){
+        editProductGroup(id:$id, name:$name){
+            name
+        }
+    }
+`;
+
 
 export default class EditPRoductGroup extends Component{
 
-    catchGroupName = e => {}
+    state = {
+        groupName: ''
+    }
 
-    saveGroup = e => {}
+    static contextType = getApolloContext(); 
+
+    catchGroupName = e => this.setState({groupName: e.target.value})
+
+    saveGroup = async ()=> {
+        const {client} = this.context;
+        const {groupName} = this.state;
+        client.mutate({
+            mutation: UPDATE_PRODUCT_GROUP,
+            variables: {
+                id: this.props.history.location.state.productGroupId,
+                name: groupName,
+            }
+        }).then(res =>{
+            console.log(res);
+            this.props.history.push('/addproductgroup');
+            window.location.reload();
+        })
+        .catch(error => console.log(error));
+    }
+
+    componentDidMount = ()=>{
+        console.log(this.props.history.location.state.productGroupId);
+    }
     
     render() {
         return (
